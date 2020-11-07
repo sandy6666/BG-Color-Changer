f97568f0d085917122cde0a821023ed076529644
Sandesh <sandesh.s@codilar.com>
2020-11-07 12:49:52 +0530

    source code

---
+++ App.js
@@ -0,0 +1,63 @@
+import React, {useState} from 'react';
+
+import {
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  const [randomColor, setRandomColor] = useState('rgb(32,0,126)');
+
+  const changeBG = () => {
+    let color =
+      'rgb(' +
+      Math.floor(Math.random() * 256) +
+      ',' +
+      Math.floor(Math.random() * 256) +
+      ',' +
+      Math.floor(Math.random() * 256) +
+      ')';
+
+    setRandomColor(color);
+  };
+
+  const changeBGToWhite = () => {
+    setRandomColor('#FFFFFF');
+  };
+  return (
+    <>
+      <StatusBar backgroundColor={randomColor} />
+      <View style={[styles.container, {backgroundColor: randomColor}]}>
+        <TouchableOpacity onPress={changeBG}>
+          <Text style={styles.text}>Tap Me</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={changeBGToWhite}>
+          <Text style={styles.text}>Change to White</Text>
+        </TouchableOpacity>
+      </View>
+    </>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
+    backgroundColor: '#BB2CD9',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    marginTop: 20,
+    color: '#FFFFFF',
+    borderRadius: 15,
+    textTransform: 'uppercase',
+  },
+});
